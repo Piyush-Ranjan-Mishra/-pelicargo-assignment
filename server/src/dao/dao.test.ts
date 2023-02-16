42e38096e1d197dd4368e4b60d74d6f66d087e3a
Piyush Mishra <piyush.mishra@loot.discount>
2023-02-16 10:24:45 +0530

    Add more server test

--- server/src/dao/dao.test.ts
+++ server/src/dao/dao.test.ts
@@ -1,8 +1,18 @@
-import { getCardsList } from "./index";
+import { getCardsList, searchCards } from "./index";
 
 describe("Pexeles", () => {
-  it("should work", async () => {
+  it("should have list", async () => {
     const result = await getCardsList(1, 1);
-    expect(result);
+    console.log({ result });
+    expect(result).toHaveProperty("photos");
+    expect(result).toHaveProperty("page");
+    expect(result).toHaveProperty("per_page");
+  });
+  it("should work search", async () => {
+    const result = await searchCards("lisa", 1, 1);
+    console.log({ result });
+    expect(result).toHaveProperty("photos");
+    expect(result).toHaveProperty("page");
+    expect(result).toHaveProperty("per_page");
   });
 });
